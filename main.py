ae1ed086c2a1ce6ca0e428a1c9077a8ada51d6ad
DavitEgoian <dato.egoyan@gmail.com>
2025-04-24 22:40:59 +0400

    Imports, as well as constants has been added

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import turtle
-import random
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from brick import Brick
+from scoreboard import Scoreboard
+
+WIDTH, HEIGHT = 800, 600
+PADDLE_MOVE = 40
+BRICK_ROWS = 4
+BRICK_COLS = 14
+COLORS = ['red', 'orange', 'yellow', 'green']
