5d63c6462d5e80b0b7e190256b77a2f603ff3adf
DavitEgoian <dato.egoyan@gmail.com>
2025-04-24 22:43:52 +0400

    Ball logic has been added

--- ball.py
+++ ball.py
@@ -1,2 +1,23 @@
+import turtle
+
+
 class Ball:
-    pass
+    def __init__(self):
+        self.t = turtle.Turtle()
+        self.t.speed(0)
+        self.t.shape('circle')
+        self.t.color('red')
+        self.t.penup()
+        self.t.goto(0, 0)
+        self.dx = 3
+        self.dy = 3
+
+    def move(self):
+        x, y = self.t.xcor() + self.dx, self.t.ycor() + self.dy
+        self.t.goto(x, y)
+
+    def bounce_x(self):
+        self.dx *= -1
+
+    def bounce_y(self):
+        self.dy *= -1
