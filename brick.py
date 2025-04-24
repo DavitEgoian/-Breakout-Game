d308487a3a86c96fb280a8bdb3e0018d7044a215
DavitEgoian <dato.egoyan@gmail.com>
2025-04-24 22:44:19 +0400

    Brick logic has been added

--- brick.py
+++ brick.py
@@ -1,2 +1,14 @@
+import turtle
+
 class Brick:
-    pass
+    def __init__(self, x, y, color):
+        self.t = turtle.Turtle()
+        self.t.speed(0)
+        self.t.shape('square')
+        self.t.color(color)
+        self.t.shapesize(stretch_wid=1, stretch_len=2)
+        self.t.penup()
+        self.t.goto(x, y)
+
+    def hit(self):
+        self.t.hideturtle()
