0dd70a651e5074f4aac593a63429be3c2424b99e
DavitEgoian <dato.egoyan@gmail.com>
2025-04-26 13:00:15 +0400

    Add paddle movement logic with arrow-key controls and boundary checks

--- paddle.py
+++ paddle.py
@@ -1,2 +1,31 @@
+import turtle
+
 class Paddle:
-    pass
+    def __init__(self, screen, width, height, move_distance):
+        self.move_distance = move_distance
+        self.screen_width = width
+        # Create paddle
+        self.t = turtle.Turtle()
+        self.t.speed(0)
+        self.t.shape('square')
+        self.t.color('white')
+        self.t.shapesize(stretch_wid=1, stretch_len=5)
+        self.t.penup()
+        start_y = -height/2 + 50
+        self.t.goto(0, start_y)
+
+    def move_left(self):
+        new_x = self.t.xcor() - self.move_distance
+        half_paddle = (5 * 20) / 2
+        left_limit = -self.screen_width/2 + half_paddle
+        if new_x < left_limit:
+            new_x = left_limit
+        self.t.goto(new_x, self.t.ycor())
+
+    def move_right(self):
+        new_x = self.t.xcor() + self.move_distance
+        half_paddle = (5 * 20) / 2
+        right_limit = self.screen_width/2 - half_paddle
+        if new_x > right_limit:
+            new_x = right_limit
+        self.t.goto(new_x, self.t.ycor())
